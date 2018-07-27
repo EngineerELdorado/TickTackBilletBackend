8669581c274f7e65e49e93e084a27480c8673871
denis.kalenga <engineereldorado@gmail.com>
2018-07-27 22:48:37 +0300

    initial commit

---
+++ src/test/java/com/example/TicTacBillet/TicTacBilletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.TicTacBillet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicTacBilletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
